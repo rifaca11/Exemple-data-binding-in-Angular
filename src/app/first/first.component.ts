ceeb98ed4d50b0fa1037b2df64f951e595324418
rifaca11 <charifah1112@gmail.com>
2023-01-09 00:07:51 +0100

    add example ngIf directive

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,15 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
+  //data binding
   user= {
     name:'charifa',
     age: 22,
     image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2cTaI9Bxuxjo-uFw7Guguka6ULJHR2BXsTQOnJMCK&s'
   }
-
   myname='';
   change(){
     this.user.name=this.myname;
+  }
+
+//  directive
+  display= true;
+
+  Visible(){
+    this.display= !this.display;
   }
 
 constructor() {}
